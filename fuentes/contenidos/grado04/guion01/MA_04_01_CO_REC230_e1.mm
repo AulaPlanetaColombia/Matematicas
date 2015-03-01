72e5867e881e3e0d9e9e0ed9899de4a457d22850
JohannaMontejo <jemontejor@pedagogica.edu.co>
2015-03-01 13:29:46 -0500

    Primera Edición

---
+++ fuentes/contenidos/grado04/guion01/MA_04_01_CO_REC230_e1.mm
@@ -0,0 +1,56 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1424751687401" ID="ID_494195941" MODIFIED="1424751700832" TEXT="Conjuntos">
+<node CREATED="1424751833830" ID="ID_675058528" MODIFIED="1424753122565" POSITION="right" TEXT="Determinaci&#xf3;n">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1424751861832" ID="ID_1392629256" MODIFIED="1424753066285" TEXT="Comprensi&#xf3;n">
+<node CREATED="1424751870662" ID="ID_697883199" MODIFIED="1424751899147" TEXT="Se nombra la o las caracter&#xed;sticas que definen al conjunto."/>
+</node>
+<node CREATED="1424753122575" ID="ID_1737709447" MODIFIED="1424753128133" TEXT="Extensi&#xf3;n ">
+<node CREATED="1424753128135" ID="ID_1324772614" MODIFIED="1424753132986" TEXT="Se nombran todos y cada uno de los elementos del conjunto. "/>
+</node>
+</node>
+<node CREATED="1424751786851" ID="ID_1477847065" MODIFIED="1424752245670" POSITION="right" TEXT="Representaci&#xf3;n ">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1424751814853" ID="ID_1433744935" MODIFIED="1424752113491" TEXT="Diagrama de Venn"/>
+</node>
+<node CREATED="1424752115987" ID="ID_816936700" MODIFIED="1424752239336" POSITION="left" TEXT="Relaciones">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1424752144301" ID="ID_992729340" MODIFIED="1424752151337" TEXT="Pertenencia">
+<node CREATED="1424752152335" ID="ID_940649128" MODIFIED="1424752308795" TEXT="Un elemento pertenece a un conjunto si cumple la o las caracter&#xed;sticas que definen al conjunto. "/>
+</node>
+<node CREATED="1424752157187" ID="ID_34003820" MODIFIED="1424752171617" TEXT="Contenencia">
+<node CREATED="1424752172880" ID="ID_1750349258" MODIFIED="1424752342866" TEXT="Un conjunto est&#xe1; contenido en otro si todos sus elementos tambi&#xe9;n pertenecen a dicho conjunto. ">
+<node CREATED="1424752344363" ID="ID_520543726" MODIFIED="1424752349402" TEXT="Subconjuntos"/>
+</node>
+</node>
+</node>
+<node CREATED="1424752362506" ID="ID_1925296618" MODIFIED="1424752639881" POSITION="left" TEXT="Clasificaci&#xf3;n">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1424752374877" ID="ID_466905351" MODIFIED="1424752391912" TEXT="Infinito">
+<node CREATED="1424752376406" ID="ID_1782151672" MODIFIED="1424752575891" TEXT="El n&#xfa;mero de elementos no tiene l&#xed;mite o fin. "/>
+</node>
+<node CREATED="1424752580131" ID="ID_1651105260" MODIFIED="1424752586228" TEXT="Finito">
+<node CREATED="1424752593965" ID="ID_1512213914" MODIFIED="1424752608050" TEXT="Se puede determinar su cardinal."/>
+</node>
+<node CREATED="1424752611522" ID="ID_723063888" MODIFIED="1424752618409" TEXT="Vac&#xed;o">
+<node CREATED="1424752618986" ID="ID_411129657" MODIFIED="1424752686541" TEXT="Cardinal = 0"/>
+</node>
+<node CREATED="1424752642600" ID="ID_1786564008" MODIFIED="1424752645278" TEXT="Unitario">
+<node CREATED="1424752659088" ID="ID_1711409841" MODIFIED="1424752678309" TEXT="Cardinal = 1"/>
+</node>
+</node>
+<node CREATED="1424752699410" ID="ID_881923968" MODIFIED="1424752754109" POSITION="right" TEXT="Operaciones">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1424752705594" ID="ID_748148609" MODIFIED="1424752719950" TEXT="Uni&#xf3;n">
+<node CREATED="1424752719954" ID="ID_1737373614" MODIFIED="1424752793856" TEXT="Agrupaci&#xf3;n de todos los elementos de los conjuntos que se unen. "/>
+</node>
+<node CREATED="1424752754111" ID="ID_1272181855" MODIFIED="1424752759776" TEXT="Intersecci&#xf3;n">
+<node CREATED="1424752759779" ID="ID_21477159" MODIFIED="1424752812859" TEXT="Agrupaci&#xf3;n de los elementos que tienen en com&#xfa;n los conjuntos que se intersectan. "/>
+</node>
+<node CREATED="1424752816846" ID="ID_513389784" MODIFIED="1424752821624" TEXT="Diferencia">
+<node CREATED="1424752821626" ID="ID_1567642052" MODIFIED="1424752863037" TEXT="Agrupaci&#xf3;n de los elementos del primer conjunto menos los elementos que tiene en com&#xfa;n con el segundo conjunto. "/>
+</node>
+</node>
+</node>
+</map>
