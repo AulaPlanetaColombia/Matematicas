079b6113c00515650d2ff04fad3d6c6387d62305
JohannaMontejo <jemontejor@pedagogica.edu.co>
2015-03-11 20:57:05 -0500

    Versión Original de la Autora

---
+++ fuentes/contenidos/grado04/guion02/MA_04_02_CO_REC380.mm
@@ -0,0 +1,170 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1426048559245" ID="ID_1394055936" MODIFIED="1426051873406" TEXT="Los n&#xfa;meros naturales (&#x2115;)">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1426048600446" ID="ID_1644894793" MODIFIED="1426051993234" POSITION="right" TEXT="Definici&#xf3;n">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="bookmark"/>
+<node COLOR="#00b439" CREATED="1426051911188" ID="ID_1355252620" MODIFIED="1426051940733" TEXT="N&#xfa;meros que indican unidades enteras">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1426048617127" ID="ID_1064485866" MODIFIED="1426051927165" TEXT="se forman">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="bookmark"/>
+<node COLOR="#111111" CREATED="1426048760790" ID="ID_75891991" MODIFIED="1426051961208" TEXT="A partir de diez d&#xed;gitos:0,1,2,3,4,5,6,7,8 y 9">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1426048797278" ID="ID_1060078702" MODIFIED="1426051982023" TEXT="de acuerdo a su posici&#xf3;n">
+<icon BUILTIN="bookmark"/>
+<node COLOR="#111111" CREATED="1426048820718" ID="ID_1783405394" MODIFIED="1426051982578" TEXT="Un grupo de diez elementos forma una unidad de orden o posici&#xf3;n mayor">
+<node COLOR="#111111" CREATED="1426048847608" ID="ID_1846447602" MODIFIED="1426051223977" TEXT="*10u=1d  *10d=1c *10c=1um *10um=1dm * 10dm=1cm *10dm=1uM *10uM=1dM *10dM=1cM"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426048962625" FOLDED="true" ID="ID_1422693782" MODIFIED="1426051223983" POSITION="left" TEXT="Ordenan y/o comparan">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="bookmark"/>
+<node COLOR="#00b439" CREATED="1426048995157" FOLDED="true" ID="ID_540780583" MODIFIED="1426051223985" TEXT="N&#xfa;mero de cifras de los n&#xfa;meros que se comparan">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1426049023178" FOLDED="true" ID="ID_1603956558" MODIFIED="1426051223986" TEXT="Igual n&#xfa;mero de cifras">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="bookmark"/>
+<node COLOR="#111111" CREATED="1426049119556" ID="ID_322324524" MODIFIED="1426051223987" TEXT="Se comparan las cifras que ocupan la misma posici&#xf3;n de izquierda a derecha hasta encontrar una desigualdad. Es mayor el n&#xfa;mero que tenga una cifra mayor en la mayor posici&#xf3;n comparada. "/>
+</node>
+<node COLOR="#990000" CREATED="1426049047876" FOLDED="true" ID="ID_151999038" MODIFIED="1426051223987" TEXT="Diferente n&#xfa;mero de cifras">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="bookmark"/>
+<node COLOR="#111111" CREATED="1426049103458" ID="ID_15646768" MODIFIED="1426051223988" TEXT="Es mayor el n&#xfa;mero que tenga mayor n&#xfa;mero de cifras"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426049213421" HGAP="42" ID="ID_520190616" MODIFIED="1426051904067" POSITION="right" TEXT="Leen y/o escriben" VSHIFT="-112">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="bookmark"/>
+<node COLOR="#00b439" CREATED="1426049227673" FOLDED="true" ID="ID_1436147688" MODIFIED="1426051223994" TEXT="Se separan las cifras en grupos de tres de derecha a izquierda y se leen o escriben por grupos.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1426049289018" FOLDED="true" ID="ID_697965900" MODIFIED="1426051223995" TEXT="Ej: 203.812.932 : Doscientos tres millones ochocientos doce mil novecientos treinta y dos. ">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1426049375701" FOLDED="true" ID="ID_801008234" MODIFIED="1426051223996" TEXT="Tener en cuenta">
+<icon BUILTIN="bookmark"/>
+<node COLOR="#111111" CREATED="1426049399935" ID="ID_1698227878" MODIFIED="1426051223996" TEXT="Ortograf&#xed;a para escribir n&#xfa;meros: 100: Cien o ciento 200: Doscientos 300: Trescientos 400: Cuatrocientos 500: Quinientos 600: Seiscientos 700: Setecientos 800: Ochocientos 900: Novecientos "/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426049507729" FOLDED="true" ID="ID_1063447428" MODIFIED="1426051224000" POSITION="left" TEXT="Representan">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="bookmark"/>
+<node COLOR="#00b439" CREATED="1426049535871" ID="ID_1457075793" MODIFIED="1426051224002" TEXT="Un punto en la recta num&#xe9;rica">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426049903802" FOLDED="true" HGAP="27" ID="ID_1317713976" MODIFIED="1426051224007" POSITION="right" TEXT="Redondean - aproximan" VSHIFT="-70">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="bookmark"/>
+<node COLOR="#00b439" CREATED="1426049922635" FOLDED="true" ID="ID_1830838561" MODIFIED="1426051224009" TEXT="1. Seleccionamos el orden o posici&#xf3;n (d, c, um, dm, cm, uM, dM o cM) a la cual se quiere redondear. 2. se localiza la cifra del orden o posici&#xf3;n elegida. 3. Se examina la cifra siguiente a la seleccionada (de orden o posici&#xf3;n menor)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1426050197508" ID="ID_342101626" MODIFIED="1426051224010" TEXT="Si es mayor que 5, se suma 1 a la cifra del orden elegido. Por &#xfa;ltimo, se sustituye con ceros todas las cifras de orden o posici&#xf3;n menores que la elegida. ">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1426050255399" ID="ID_1443010521" MODIFIED="1426051224011" TEXT="Si es menor o igual que 5, se deja la cifra del orden elegido como estaba. Por &#xfa;ltimo, se sustituye con ceros todas las cifras de orden o posici&#xf3;n menores que la elegida. ">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426050335319" FOLDED="true" ID="ID_1880174206" MODIFIED="1426051695679" POSITION="left" TEXT="Operaciones aritm&#xe9;ticas">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="bookmark"/>
+<node COLOR="#00b439" CREATED="1426050355536" FOLDED="true" ID="ID_678897129" MODIFIED="1426051224023" TEXT="Adici&#xf3;n">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1426050466258" FOLDED="true" ID="ID_327590497" MODIFIED="1426051224024" TEXT="Propiedades">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="bookmark"/>
+<node COLOR="#111111" CREATED="1426050479006" FOLDED="true" ID="ID_750009717" MODIFIED="1426051224025" TEXT="Modulativa">
+<node COLOR="#111111" CREATED="1426050502922" FOLDED="true" ID="ID_470713938" MODIFIED="1426051224025" TEXT="Cualquier n&#xfa;mero sumado con cero da como resultado el mismo n&#xfa;mero.">
+<node COLOR="#111111" CREATED="1426050592394" ID="ID_675394937" MODIFIED="1426051224026" TEXT="Elemento neutro: cero (0)"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1426050486541" FOLDED="true" ID="ID_806898143" MODIFIED="1426051224026" TEXT="Conmutativa">
+<node COLOR="#111111" CREATED="1426050545354" ID="ID_1558750622" MODIFIED="1426051224027" TEXT="El orden de los sumandos no cambia el resultado"/>
+</node>
+<node COLOR="#111111" CREATED="1426050493177" FOLDED="true" ID="ID_220259994" MODIFIED="1426051224027" TEXT="Asociativa">
+<node COLOR="#111111" CREATED="1426050625122" ID="ID_419541244" MODIFIED="1426051224028" TEXT="La forma como se asocien los sumandos no cambian el resultado"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1426050430441" ID="ID_663406425" MODIFIED="1426051224035" TEXT="Sustracci&#xf3;n">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1426050436871" FOLDED="true" ID="ID_1793323899" MODIFIED="1426051224039" TEXT="Multiplicaci&#xf3;n">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1426050683754" FOLDED="true" ID="ID_1586814420" MODIFIED="1426051224040" TEXT="Propiedades">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="bookmark"/>
+<node COLOR="#111111" CREATED="1426050700728" FOLDED="true" ID="ID_1140483472" MODIFIED="1426051224041" TEXT="Modulativa">
+<node COLOR="#111111" CREATED="1426050727746" FOLDED="true" ID="ID_1007696133" MODIFIED="1426051224041" TEXT="Cualquier n&#xfa;mero multiplicado por uno da como producto el mismo n&#xfa;mero.">
+<node COLOR="#111111" CREATED="1426050762020" ID="ID_1165459328" MODIFIED="1426051224042" TEXT="Elemento neutro: uno (1)"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1426050705196" FOLDED="true" ID="ID_824728123" MODIFIED="1426051224042" TEXT="Conmutativa">
+<node COLOR="#111111" CREATED="1426050777035" ID="ID_413971658" MODIFIED="1426051224043" TEXT="El orden de los factores no cambia el producto"/>
+</node>
+<node COLOR="#111111" CREATED="1426050708468" FOLDED="true" ID="ID_367041571" MODIFIED="1426051224043" TEXT="Asociativa">
+<node COLOR="#111111" CREATED="1426050804859" ID="ID_371107818" MODIFIED="1426051224044" TEXT="La forma como se asocien los factores no cambia el producto"/>
+</node>
+<node COLOR="#111111" CREATED="1426050710942" FOLDED="true" ID="ID_1750458468" MODIFIED="1426051224044" TEXT="Distributiva">
+<node COLOR="#111111" CREATED="1426050828010" ID="ID_1536010480" MODIFIED="1426051224045" TEXT="Se aplica cuando se multiplica un n&#xfa;mero a una adici&#xf3;n. "/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1426050441694" ID="ID_572053152" MODIFIED="1426051224048" TEXT="Divisi&#xf3;n">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1426050448186" ID="ID_1728915443" MODIFIED="1426051224054" TEXT="Operaciones combinadas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1426050863240" ID="ID_955990571" MODIFIED="1426051224056" TEXT="Se resuelven con el orden">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="bookmark"/>
+<node COLOR="#111111" CREATED="1426050985552" ID="ID_1919767746" MODIFIED="1426051224066" TEXT="1. Resolvemos las operaciones que hay dentro de los par&#xe9;ntesis. 2. Resolvemos las multiplicaciones y las divisiones en el orden en que aparezcan, de izquierda a derecha. 3. Hacemos las sumas y restas en el orden en que aparezcan, de izquierda a derecha. "/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1426051252069" ID="ID_1054888313" MODIFIED="1426052157503" POSITION="right" TEXT="Diferentes sistemas num&#xe9;ricos">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1426051263828" ID="ID_774074413" MODIFIED="1426051651602" TEXT="Sistema de numeraci&#xf3;n egipcio">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1426052184021" ID="ID_1325301337" MODIFIED="1426052204301" TEXT="(Quiero poner la imagen de los s&#xed;mbolos de los egipcio)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1426051634983" ID="ID_749903695" MODIFIED="1426051643936" TEXT="Sistema de numeraci&#xf3;n romano">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1426052210794" ID="ID_681060548" MODIFIED="1426052227010" TEXT="(Quiero poner la imagen de los n&#xfa;meros romanos)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
