c215d12904c08668bbaabe62b7a54533190afd4e
niviaromero <lufeniro@gmail.com>
2015-03-16 10:57:46 -0500

    Edición.2

--- fuentes/contenidos/grado05/guion01/MA_05_01_CO_REC240.mm
+++ fuentes/contenidos/grado05/guion01/MA_05_01_CO_REC240.mm
@@ -4,27 +4,36 @@
 <node CREATED="1424884584977" ID="ID_373446164" MODIFIED="1425782223513" POSITION="right" TEXT="se concretan en ">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1424884616515" ID="ID_1907808562" MODIFIED="1424884823292" STYLE="fork" TEXT="Concepto">
-<node CREATED="1424884660992" ID="ID_207723865" MODIFIED="1425782144326" TEXT="se define como">
+<node CREATED="1424884660992" FOLDED="true" ID="ID_207723865" MODIFIED="1426474321545" TEXT="se define como">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
 <node CREATED="1424885165924" ID="ID_1652870320" MODIFIED="1425782148996" TEXT="Colecci&#xf3;n de elementos con caracter&#xed;sticas en com&#xfa;n."/>
 </node>
 </node>
 <node CREATED="1424884689538" ID="ID_1040379793" MODIFIED="1424884990493" STYLE="fork" TEXT="Representaci&#xf3;n">
-<node CREATED="1424885181343" ID="ID_953134207" MODIFIED="1425781572641" TEXT="por medio de">
+<node CREATED="1424885181343" ID="ID_953134207" MODIFIED="1426474395380" TEXT="por medio de">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1424885194732" ID="ID_1180271897" MODIFIED="1425782169976" TEXT="Diagramas de Venn">
+<icon BUILTIN="idea"/>
+<node CREATED="1424885194732" ID="ID_1180271897" MODIFIED="1426474406377" TEXT="Diagramas de Venn">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1425607141844" ID="_" MODIFIED="1425607171350" TEXT="Se encierran los elementos en una l&#xed;nea cerrada"/>
+<node CREATED="1426474406295" ID="ID_144069842" MODIFIED="1426474562052" TEXT="consiste en">
+<icon BUILTIN="idea"/>
+<node CREATED="1425607141844" ID="_" MODIFIED="1426474599586" TEXT="Encerrar los elementos con una l&#xed;nea cerrada."/>
 </node>
-<node CREATED="1424885206176" ID="ID_1889554975" MODIFIED="1425782177733" TEXT="Llaves">
+</node>
+<node CREATED="1424885206176" ID="ID_1889554975" MODIFIED="1426474454290" TEXT="Llaves">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1425607178553" ID="Freemind_Link_210343214" MODIFIED="1425782160104" TEXT="se escriben los elementos entre corchetes"/>
+<node CREATED="1426474454261" ID="ID_1232624363" MODIFIED="1426474498867" TEXT="consiste en">
+<icon BUILTIN="idea"/>
+<node CREATED="1425607178553" ID="Freemind_Link_210343214" MODIFIED="1426474556140" TEXT="Escribir los elementos entre llaves."/>
+</node>
 </node>
 </node>
 </node>
 <node CREATED="1424884701488" ID="ID_1081811694" MODIFIED="1425608018479" TEXT="Determinaci&#xf3;n">
-<node CREATED="1424885222242" ID="ID_1595416953" MODIFIED="1425781576854" TEXT="se escriben">
+<node CREATED="1424885222242" FOLDED="true" ID="ID_1595416953" MODIFIED="1426474326458" TEXT="se escriben">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
 <node CREATED="1424885248485" ID="ID_1134822164" MODIFIED="1425782174369" TEXT="Por extensi&#xf3;n">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1424885255691" ID="ID_359903860" MODIFIED="1425781850269" TEXT="si y solo si">
@@ -42,40 +51,46 @@
 </node>
 </node>
 <node CREATED="1424884857851" HGAP="23" ID="ID_798125870" MODIFIED="1425608502867" TEXT="Operaciones" VSHIFT="2">
-<node CREATED="1424885426230" ID="ID_1648599769" MODIFIED="1425781580743" TEXT="se identifica">
+<node CREATED="1424885426230" ID="ID_1648599769" MODIFIED="1426474328848" TEXT="se identifica">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1424885449210" FOLDED="true" ID="ID_1437493014" MODIFIED="1425782180182" TEXT="La uni&#xf3;n">
+<icon BUILTIN="idea"/>
+<node CREATED="1424885449210" ID="ID_1437493014" MODIFIED="1426474347813" TEXT="La uni&#xf3;n">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1425607238431" ID="Freemind_Link_604917303" MODIFIED="1425781817556" TEXT="para los conjuntos A y B">
+<node CREATED="1425607238431" ID="Freemind_Link_604917303" MODIFIED="1426474350179" TEXT="para los conjuntos A y B">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
 <node CREATED="1425608048338" ID="Freemind_Link_1413712146" MODIFIED="1425782008053" TEXT="{x/x pertenece a A o x pertenece a B}"/>
 </node>
 </node>
 <node CREATED="1424885452921" ID="ID_995601476" MODIFIED="1425782182428" TEXT="La intersecci&#xf3;n">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1425608081301" ID="Freemind_Link_1312439292" MODIFIED="1425781822010" TEXT="para los conjuntos A y B    ">
+<node CREATED="1425608081301" ID="Freemind_Link_1312439292" MODIFIED="1426474339999" TEXT="para los conjuntos A y B    ">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
 <node CREATED="1425608100182" ID="Freemind_Link_822666101" MODIFIED="1425608120767" TEXT="{x/x pertenece a A y x pertenece a B}"/>
 </node>
 </node>
 <node CREATED="1424885455875" ID="ID_669277369" MODIFIED="1425782188026" TEXT="La diferencia">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1425608128029" ID="Freemind_Link_1091711849" MODIFIED="1425781828135" TEXT="para los conjuntos A y B ">
+<node CREATED="1425608128029" ID="Freemind_Link_1091711849" MODIFIED="1426474342523" TEXT="para los conjuntos A y B ">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
 <node CREATED="1425608145613" ID="Freemind_Link_1771640525" MODIFIED="1425608163482" TEXT="{x/x pertenece a A y x no pertenece a B}"/>
 </node>
 </node>
-<node CREATED="1424885458727" FOLDED="true" ID="ID_1869589257" MODIFIED="1425782189901" TEXT="La diferencia sim&#xe9;trica">
+<node CREATED="1424885458727" ID="ID_1869589257" MODIFIED="1426474351767" TEXT="La diferencia sim&#xe9;trica">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1425608185113" ID="Freemind_Link_261132608" MODIFIED="1425781843952" TEXT="para los conjuntos A y B ">
+<node CREATED="1425608185113" ID="Freemind_Link_261132608" MODIFIED="1426474354109" TEXT="para los conjuntos A y B ">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
 <node CREATED="1425608212757" ID="Freemind_Link_1395443661" MODIFIED="1425608313417" TEXT="{x/x pertenece a (A - B) o x pertenece a (B - A)}"/>
 </node>
 </node>
-<node CREATED="1424885460980" FOLDED="true" ID="ID_1171517317" MODIFIED="1425782192563" TEXT="El complemento">
+<node CREATED="1424885460980" ID="ID_1171517317" MODIFIED="1426474355600" TEXT="El complemento">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1425608323116" ID="Freemind_Link_39343849" MODIFIED="1425781844998" TEXT="para el conjunto A ">
+<node CREATED="1425608323116" ID="Freemind_Link_39343849" MODIFIED="1426474358086" TEXT="para el conjunto A ">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
 <node CREATED="1425608436163" ID="Freemind_Link_1793475100" MODIFIED="1425608461895" TEXT="{x/x pertenece al conjunto universal y x no pertenece a A}"/>
 </node>
 </node>
@@ -94,7 +109,7 @@
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1424885786664" ID="ID_681673396" MODIFIED="1424885797280" TEXT="Conjunto que tiene un solo elemento"/>
 </node>
-<node CREATED="1424885568844" ID="ID_661580977" MODIFIED="1425782201678" TEXT="Vacio">
+<node CREATED="1424885568844" ID="ID_661580977" MODIFIED="1426474663587" TEXT="Vac&#xed;o">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1424885803203" ID="ID_1826589161" MODIFIED="1424885823246" TEXT="Conjunto que no tiene elementos"/>
 </node>
@@ -104,12 +119,14 @@
 </node>
 </node>
 <node CREATED="1424885082922" ID="ID_1162473234" MODIFIED="1424885089548" TEXT="Relaciones">
-<node CREATED="1424885935339" ID="ID_749889993" MODIFIED="1425781881443" TEXT="entre elemento y conjunto">
+<node CREATED="1424885935339" ID="ID_749889993" MODIFIED="1426474332502" TEXT="entre elemento y conjunto">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
 <node CREATED="1424885964383" ID="ID_1707352873" MODIFIED="1424885975205" TEXT="Pertenencia"/>
 </node>
-<node CREATED="1424885979959" ID="ID_1833098080" MODIFIED="1425781953878" TEXT="entre conjunto y conjunto">
+<node CREATED="1424885979959" ID="ID_1833098080" MODIFIED="1426474335037" TEXT="entre conjunto y conjunto">
 <font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
 <node CREATED="1424885991810" ID="ID_158175595" MODIFIED="1425782208295" TEXT="Contenencia">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1424886045340" ID="ID_1787114297" MODIFIED="1425782044818" TEXT="Un conjunto es subconjunto del otro"/>
