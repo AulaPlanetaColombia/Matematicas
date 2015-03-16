39d534cb05cf5ceb31266633206e606553ea3b1b
niviaromero <lufeniro@gmail.com>
2015-03-16 10:21:03 -0500

    Edición.1

---
+++ fuentes/contenidos/grado05/guion01/MA_05_01_CO_REC240.mm
@@ -0,0 +1,133 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1424884221105" ID="ID_1199955085" MODIFIED="1424884947709" TEXT="Conjuntos">
+<node CREATED="1424884584977" ID="ID_373446164" MODIFIED="1425782223513" POSITION="right" TEXT="se concretan en ">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424884616515" ID="ID_1907808562" MODIFIED="1424884823292" STYLE="fork" TEXT="Concepto">
+<node CREATED="1424884660992" ID="ID_207723865" MODIFIED="1425782144326" TEXT="se define como">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885165924" ID="ID_1652870320" MODIFIED="1425782148996" TEXT="Colecci&#xf3;n de elementos con caracter&#xed;sticas en com&#xfa;n."/>
+</node>
+</node>
+<node CREATED="1424884689538" ID="ID_1040379793" MODIFIED="1424884990493" STYLE="fork" TEXT="Representaci&#xf3;n">
+<node CREATED="1424885181343" ID="ID_953134207" MODIFIED="1425781572641" TEXT="por medio de">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885194732" ID="ID_1180271897" MODIFIED="1425782169976" TEXT="Diagramas de Venn">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425607141844" ID="_" MODIFIED="1425607171350" TEXT="Se encierran los elementos en una l&#xed;nea cerrada"/>
+</node>
+<node CREATED="1424885206176" ID="ID_1889554975" MODIFIED="1425782177733" TEXT="Llaves">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425607178553" ID="Freemind_Link_210343214" MODIFIED="1425782160104" TEXT="se escriben los elementos entre corchetes"/>
+</node>
+</node>
+</node>
+<node CREATED="1424884701488" ID="ID_1081811694" MODIFIED="1425608018479" TEXT="Determinaci&#xf3;n">
+<node CREATED="1424885222242" ID="ID_1595416953" MODIFIED="1425781576854" TEXT="se escriben">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885248485" ID="ID_1134822164" MODIFIED="1425782174369" TEXT="Por extensi&#xf3;n">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885255691" ID="ID_359903860" MODIFIED="1425781850269" TEXT="si y solo si">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885289355" ID="ID_1886886177" MODIFIED="1425298789045" TEXT="Todos sus elementos se escriben entre llaves"/>
+</node>
+</node>
+<node CREATED="1424885266269" ID="ID_953959484" MODIFIED="1425782176421" TEXT="Por comprensi&#xf3;n">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885337747" ID="ID_288293089" MODIFIED="1425781806653" TEXT="si">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885342355" ID="ID_1595715021" MODIFIED="1424885363709" TEXT="Se escribe entre llaves una caracter&#xed;stica que identifica todos los elementos del cionjunto"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1424884857851" HGAP="23" ID="ID_798125870" MODIFIED="1425608502867" TEXT="Operaciones" VSHIFT="2">
+<node CREATED="1424885426230" ID="ID_1648599769" MODIFIED="1425781580743" TEXT="se identifica">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885449210" FOLDED="true" ID="ID_1437493014" MODIFIED="1425782180182" TEXT="La uni&#xf3;n">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425607238431" ID="Freemind_Link_604917303" MODIFIED="1425781817556" TEXT="para los conjuntos A y B">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425608048338" ID="Freemind_Link_1413712146" MODIFIED="1425782008053" TEXT="{x/x pertenece a A o x pertenece a B}"/>
+</node>
+</node>
+<node CREATED="1424885452921" ID="ID_995601476" MODIFIED="1425782182428" TEXT="La intersecci&#xf3;n">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425608081301" ID="Freemind_Link_1312439292" MODIFIED="1425781822010" TEXT="para los conjuntos A y B    ">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425608100182" ID="Freemind_Link_822666101" MODIFIED="1425608120767" TEXT="{x/x pertenece a A y x pertenece a B}"/>
+</node>
+</node>
+<node CREATED="1424885455875" ID="ID_669277369" MODIFIED="1425782188026" TEXT="La diferencia">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425608128029" ID="Freemind_Link_1091711849" MODIFIED="1425781828135" TEXT="para los conjuntos A y B ">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425608145613" ID="Freemind_Link_1771640525" MODIFIED="1425608163482" TEXT="{x/x pertenece a A y x no pertenece a B}"/>
+</node>
+</node>
+<node CREATED="1424885458727" FOLDED="true" ID="ID_1869589257" MODIFIED="1425782189901" TEXT="La diferencia sim&#xe9;trica">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425608185113" ID="Freemind_Link_261132608" MODIFIED="1425781843952" TEXT="para los conjuntos A y B ">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425608212757" ID="Freemind_Link_1395443661" MODIFIED="1425608313417" TEXT="{x/x pertenece a (A - B) o x pertenece a (B - A)}"/>
+</node>
+</node>
+<node CREATED="1424885460980" FOLDED="true" ID="ID_1171517317" MODIFIED="1425782192563" TEXT="El complemento">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425608323116" ID="Freemind_Link_39343849" MODIFIED="1425781844998" TEXT="para el conjunto A ">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425608436163" ID="Freemind_Link_1793475100" MODIFIED="1425608461895" TEXT="{x/x pertenece al conjunto universal y x no pertenece a A}"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1424885055304" ID="ID_1991235337" MODIFIED="1425608503377" TEXT="Clases">
+<node CREATED="1424885070851" ID="ID_422082670" MODIFIED="1425782228422" TEXT="Finito">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885686695" ID="ID_270139278" MODIFIED="1424885749012" TEXT="Aquel en el que se pueden contar todos sus elementos "/>
+</node>
+<node CREATED="1424885563202" ID="ID_513753553" MODIFIED="1425782197068" TEXT="Infinito">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885755743" ID="ID_1376008878" MODIFIED="1424885778087" TEXT="Conjunto en el que el proceso de contar sus elementos no termina"/>
+</node>
+<node CREATED="1424885566649" ID="ID_1275088198" MODIFIED="1425782230225" TEXT="Unitario">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885786664" ID="ID_681673396" MODIFIED="1424885797280" TEXT="Conjunto que tiene un solo elemento"/>
+</node>
+<node CREATED="1424885568844" ID="ID_661580977" MODIFIED="1425782201678" TEXT="Vacio">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885803203" ID="ID_1826589161" MODIFIED="1424885823246" TEXT="Conjunto que no tiene elementos"/>
+</node>
+<node CREATED="1424885580057" ID="ID_1199312437" MODIFIED="1425782204153" TEXT="Universal">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885825443" ID="ID_290047271" MODIFIED="1424885843414" TEXT="Conjunto que se toma como referencia para formar otros subconjuntos"/>
+</node>
+</node>
+<node CREATED="1424885082922" ID="ID_1162473234" MODIFIED="1424885089548" TEXT="Relaciones">
+<node CREATED="1424885935339" ID="ID_749889993" MODIFIED="1425781881443" TEXT="entre elemento y conjunto">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885964383" ID="ID_1707352873" MODIFIED="1424885975205" TEXT="Pertenencia"/>
+</node>
+<node CREATED="1424885979959" ID="ID_1833098080" MODIFIED="1425781953878" TEXT="entre conjunto y conjunto">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424885991810" ID="ID_158175595" MODIFIED="1425782208295" TEXT="Contenencia">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424886045340" ID="ID_1787114297" MODIFIED="1425782044818" TEXT="Un conjunto es subconjunto del otro"/>
+</node>
+<node CREATED="1424885999717" ID="ID_1612454590" MODIFIED="1425782253772" TEXT="Disjunci&#xf3;n">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1425608545891" ID="Freemind_Link_1881050690" MODIFIED="1425608557818" TEXT="Los conjuntos no tienen elementos en com&#xfa;n"/>
+</node>
+<node CREATED="1424886010270" ID="ID_756123187" MODIFIED="1425782214048" TEXT="Igualdad">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424886130730" ID="ID_344224823" MODIFIED="1424886145792" TEXT="Los conjuntos tienen los mismos elementos"/>
+</node>
+<node CREATED="1424886018088" ID="ID_67522614" MODIFIED="1425782216714" TEXT="Intersecantes">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424886110878" ID="ID_1993533069" MODIFIED="1424886126462" TEXT="Los conjuntos tienen elementos comunes"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
