41be0520df8740105ddf205f2e0d3c63557f7116
ejmalagon <ejmalagon@gmail.com>
2015-04-08 11:13:23 -0500

    Corrección de estilo tema 1 grado 10

--- fuentes/contenidos/grado10/guion01/MA_10_01_CO_REC230.mm
+++ fuentes/contenidos/grado10/guion01/MA_10_01_CO_REC230.mm
@@ -3,60 +3,62 @@
 <node CREATED="1426236276396" ID="ID_1914309581" MODIFIED="1426552720994" TEXT="Funci&#xf3;n">
 <icon BUILTIN="full-1"/>
 <node CREATED="1426236304039" ID="ID_398628318" MODIFIED="1426553270627" POSITION="right" TEXT="Definici&#xf3;n">
-<node CREATED="1426236609415" ID="ID_1278965456" MODIFIED="1426553381785" TEXT="Relaci&#xf3;n entre dos conjuntos tal que que a cada elemento del primer conjunto le corresponde uno y solo un elemento del segundo conjunto"/>
+<node CREATED="1426236609415" ID="ID_1278965456" MODIFIED="1428030602536" TEXT="Relaci&#xf3;n entre dos conjuntos tal que a cada elemento del primer conjunto le corresponde uno y solo uno de los elemento del segundo conjunto."/>
 </node>
 <node COLOR="#006699" CREATED="1426236376611" ID="ID_1221528972" MODIFIED="1426236534628" POSITION="left" TEXT="Representaciones">
 <node CREATED="1426236403307" ID="ID_813688711" MODIFIED="1426236510526" TEXT="Relacional">
-<node CREATED="1426552737686" ID="ID_239298105" MODIFIED="1426553002602" TEXT="Es la expresi&#xf3;n que indica la forma de relaci&#xf3;n entre las variables"/>
+<node CREATED="1426552737686" ID="ID_239298105" MODIFIED="1428029970118" TEXT="Es la expresi&#xf3;n que indica la forma de relaci&#xf3;n entre las variables."/>
 </node>
 <node CREATED="1426236467940" ID="ID_411538472" MODIFIED="1426236520291" TEXT="Conjuntista">
-<node CREATED="1426553008468" ID="ID_1533187838" MODIFIED="1426553053833" TEXT="Es la correspondencia entre los elementos del dominio y del codominio y las flechas que los unen"/>
+<node CREATED="1426553008468" ID="ID_1533187838" MODIFIED="1428030523142" TEXT="Es la correspondencia entre los elementos del dominio, del codominio y las flechas que los unen."/>
 </node>
 <node CREATED="1426236494441" ID="ID_1011471329" MODIFIED="1426236525346" TEXT="Tabular">
-<node CREATED="1426553059683" ID="ID_1307842161" MODIFIED="1426553101928" TEXT="Es la tabla de valores que consolida las parejas ordenadas de la representaci&#xf3;n conjuntista"/>
+<node CREATED="1426553059683" ID="ID_1307842161" MODIFIED="1428029985185" TEXT="Es la tabla de valores que consolida las parejas ordenadas de la representaci&#xf3;n conjuntista."/>
 </node>
 <node CREATED="1426236528793" ID="ID_534680337" MODIFIED="1426236533473" TEXT="Gr&#xe1;fica">
-<node CREATED="1426553107591" ID="ID_562036733" MODIFIED="1426553135109" TEXT="Es el conjunto de puntos que corresponde a la imagen de la funci&#xf3;n"/>
+<node CREATED="1426553107591" ID="ID_562036733" MODIFIED="1428029988310" TEXT="Es el conjunto de puntos que corresponde a la imagen de la funci&#xf3;n."/>
 </node>
 <node CREATED="1426236537233" ID="ID_417191994" MODIFIED="1426236540712" TEXT="Anal&#xed;tica">
-<node CREATED="1426553139727" ID="ID_1804871257" MODIFIED="1426553201284" TEXT="Es la ecuaci&#xf3;n que resume la relaci&#xf3;n de dependencia entre las variables de manera general"/>
+<node CREATED="1426553139727" ID="ID_1804871257" MODIFIED="1428029990695" TEXT="Es la ecuaci&#xf3;n que resume la relaci&#xf3;n de dependencia entre las variables de manera general."/>
 </node>
 </node>
 <node CREATED="1426236391041" ID="ID_1827676286" MODIFIED="1426236399080" POSITION="right" TEXT="Propiedades">
 <node CREATED="1426553393266" ID="ID_47575661" MODIFIED="1426553469129" TEXT="Seg&#xfa;n sus formas de saturaci&#xf3;n">
 <node CREATED="1426553454106" ID="ID_1401765278" MODIFIED="1426553467710" TEXT="Inyectiva">
-<node CREATED="1426553493278" ID="ID_328109644" MODIFIED="1426553677414" TEXT="A cada elemento del dominio lo env&#xed;a en un elemento distinto del codominio."/>
-<node CREATED="1426553680456" ID="ID_1224626194" MODIFIED="1426553707928" TEXT="Criterio de decisi&#xf3;n: Criterio de la recta horizontal"/>
+<node CREATED="1426553493278" ID="ID_328109644" MODIFIED="1428029567771" TEXT="A cada elemento del dominio se conecta con un elemento distinto del codominio."/>
+<node CREATED="1426553680456" ID="ID_1224626194" MODIFIED="1428029604640" TEXT="Criterio de decisi&#xf3;n: criterio de la recta horizontal."/>
 </node>
 <node CREATED="1426553471719" ID="ID_499005815" MODIFIED="1426553481157" TEXT="Sobreyectiva">
-<node CREATED="1426553712732" ID="ID_956082749" MODIFIED="1426553761873" TEXT="A todos los elementos del codominio llega una flecha desde el dominio"/>
-<node CREATED="1426553766194" ID="ID_424151458" MODIFIED="1426553801107" TEXT="Criterio de decisi&#xf3;n. No sobran elementos en el codominio"/>
+<node CREATED="1426553712732" ID="ID_956082749" MODIFIED="1428029609277" TEXT="A todos los elementos del codominio llega una flecha desde el dominio."/>
+<node CREATED="1426553766194" ID="ID_424151458" MODIFIED="1428030870234" TEXT="Criterio de decisi&#xf3;n: no sobran elementos en el codominio."/>
 </node>
 <node CREATED="1426553483544" ID="ID_592708154" MODIFIED="1426553489050" TEXT="Biyectiva">
-<node CREATED="1426553811122" ID="ID_1330276520" MODIFIED="1426553844771" TEXT="Entre los elementos del dominio y del codominio hay una &#xfa;nica flecha, sin que falten o sobren elementos en cada conjunto"/>
-<node CREATED="1426553851245" ID="ID_1372177784" MODIFIED="1426553882539" TEXT="Criterio de decisi&#xf3;n: Criterio simult&#xe1;neo de la recta horizontal y de la recta vertical"/>
+<node CREATED="1426553811122" ID="ID_1330276520" MODIFIED="1428029654553" TEXT="Entre los elementos del dominio y del codominio hay una &#xfa;nica flecha, sin que falten o sobren elementos en cada conjunto."/>
+<node CREATED="1426553851245" ID="ID_1372177784" MODIFIED="1428029648873" TEXT="Criterio de decisi&#xf3;n: criterio simult&#xe1;neo de la recta horizontal y de la recta vertical."/>
 </node>
 </node>
 <node CREATED="1426553411924" ID="ID_653012566" MODIFIED="1426553912014" TEXT="Seg&#xfa;n su crecimiento">
 <node CREATED="1426553890557" ID="ID_1605282262" MODIFIED="1426553902326" TEXT="Constante">
-<node CREATED="1426553923527" ID="ID_169547267" MODIFIED="1426554046174" TEXT="y=f(x)=a. Funci&#xf3;n paralela al eje X, que no cambia su valor en el codominio"/>
+<node CREATED="1426553923527" ID="ID_169547267" MODIFIED="1428031189421" TEXT="y = f(x) = a. Funci&#xf3;n paralela al eje X, que no cambia su valor en el codominio.">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
 </node>
 <node CREATED="1426553904417" ID="ID_425503030" MODIFIED="1426553907740" TEXT="Creciente">
-<node CREATED="1426553998423" ID="ID_323598517" MODIFIED="1426554102620" TEXT="Si x_1&lt;x_2, entonces tambi&#xe9;n  f( x_1 )&lt; f( x_2 ). Cambios ascendentes en elementos del dominio implican a su vez cambios ascendentes en los elementos del codominio"/>
+<node CREATED="1426553998423" ID="ID_323598517" MODIFIED="1428031301974" TEXT="Si x_1 &lt; x_2, entonces tambi&#xe9;n  f( x_1 ) &lt; f( x_2 ). Cambios ascendentes en elementos del dominio implican, a su vez, cambios ascendentes en los elementos del codominio."/>
 </node>
 <node CREATED="1426553912014" ID="ID_1898400001" MODIFIED="1426553918691" TEXT="Decreciente">
-<node CREATED="1426554115131" ID="ID_992823772" MODIFIED="1426554182407" TEXT="Si x_1&lt;x_2, entonces  f( x_1 )&gt; f( x_2 ). Cambios ascendentes en elementos del dominio implican a su vez cambios descendentes en los elementos del codominio"/>
+<node CREATED="1426554115131" ID="ID_992823772" MODIFIED="1428031417236" TEXT="Si x_1 &lt; x_2, entonces  f(x_1) &gt; f(x_2). Cambios ascendentes en elementos del dominio implican, a su vez, cambios descendentes en los elementos del codominio."/>
 </node>
 </node>
 <node CREATED="1426553429755" ID="ID_194349817" MODIFIED="1426554198538" TEXT="Seg&#xfa;n su simetr&#xed;a">
 <node CREATED="1426554188772" ID="ID_1457976357" MODIFIED="1426554195855" TEXT="Par">
-<node CREATED="1426554224730" ID="ID_1963171129" MODIFIED="1426554267833" TEXT="f(x)=f(-x). Sim&#xe9;trica respecto al eje x"/>
+<node CREATED="1426554224730" ID="ID_1963171129" MODIFIED="1428031557710" TEXT="f(x) = f(-x). Sim&#xe9;trica respecto al eje X."/>
 </node>
 <node CREATED="1426554198538" ID="ID_472167865" MODIFIED="1426554201783" TEXT="Impar">
-<node CREATED="1426554278956" ID="ID_1601338677" MODIFIED="1426554294493" TEXT="f(x)=-f(-x). Sim&#xe9;trica respecto al origen"/>
+<node CREATED="1426554278956" ID="ID_1601338677" MODIFIED="1428031568970" TEXT="f(x) = -f(-x). Sim&#xe9;trica respecto al origen."/>
 </node>
 <node CREATED="1426554207243" ID="ID_1290443938" MODIFIED="1426554212609" TEXT="Peri&#xf3;dica">
-<node CREATED="1426554298783" ID="ID_392283598" MODIFIED="1426554343290" TEXT="f(x)=f(x)+p. Copias equidistantes de la misma funci&#xf3;n"/>
+<node CREATED="1426554298783" ID="ID_392283598" MODIFIED="1428031583131" TEXT="f(x) = f(x) + p. Copias equidistantes de la misma funci&#xf3;n."/>
 </node>
 </node>
 </node>
