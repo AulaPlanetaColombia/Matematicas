9d1f4cff4526126f09b7bea75e175b9445b0720b
niviaromero <lufeniro@gmail.com>
2015-04-27 10:12:47 -0500

    MA_05_03. Formulas

--- fuentes/contenidos/grado05/guion03/MA_05_03_REC290/MA_05_03_REC290.mm
+++ fuentes/contenidos/grado05/guion03/MA_05_03_REC290/MA_05_03_REC290.mm
@@ -1,14 +1,14 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1424884221105" ID="ID_1199955085" MODIFIED="1428510269660" TEXT="La potenciaci&#xf3;n, la radicaci&#xf3;n y la logaritmaci&#xf3;n de n&#xfa;meros naturales">
-<node CREATED="1424884584977" ID="ID_373446164" MODIFIED="1428510309219" POSITION="right" TEXT="se definen">
+<node CREATED="1424884584977" ID="ID_373446164" MODIFIED="1428712650673" POSITION="right" TEXT="se encuentra">
 <icon BUILTIN="idea"/>
 <node CREATED="1424884701488" ID="ID_1081811694" MODIFIED="1427544267900" TEXT="Potenciaci&#xf3;n">
-<node CREATED="1427340312390" ID="Freemind_Link_1349098415" MODIFIED="1427340426156" TEXT="definici&#xf3;n">
+<node CREATED="1427340312390" ID="Freemind_Link_1349098415" MODIFIED="1428712662485" TEXT="se define">
 <icon BUILTIN="idea"/>
 <node CREATED="1427340392316" ID="Freemind_Link_1140502343" MODIFIED="1428510804582" TEXT="forma abreviada de un producto de factores iguales."/>
 </node>
-<node CREATED="1427865221562" ID="Freemind_Link_823416171" MODIFIED="1427865582774" TEXT="s&#xed;mbolizaci&#xf3;n">
+<node CREATED="1427865221562" ID="Freemind_Link_823416171" MODIFIED="1428712676022" TEXT="se simboliza">
 <icon BUILTIN="idea"/>
 <node CREATED="1427865722287" ID="Freemind_Link_812409956" MODIFIED="1428510910377">
 <richcontent TYPE="NODE"><html>
@@ -18,8 +18,7 @@
   <body>
     <img src="Potencia.png" />
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1425856728725" ID="Freemind_Link_1092842780" MODIFIED="1428510800691" TEXT="sus t&#xe9;rminos son">
@@ -28,13 +27,11 @@
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 <node CREATED="1427340074202" ID="Freemind_Link_695236852" MODIFIED="1428510419770" TEXT="exponente, n"/>
-<node CREATED="1427340086583" FOLDED="true" ID="Freemind_Link_1476989835" MODIFIED="1428510378011" TEXT="potencia. b">
-<node CREATED="1427865763220" ID="Freemind_Link_1884470848" MODIFIED="1427865766205" TEXT="b"/>
+<node CREATED="1427340086583" ID="Freemind_Link_1476989835" MODIFIED="1430140081420" TEXT="potencia. b"/>
 </node>
-</node>
-<node CREATED="1427340532976" ID="Freemind_Link_1852838837" MODIFIED="1427340555541" TEXT="potencias especiales">
+<node CREATED="1427340532976" ID="Freemind_Link_1852838837" MODIFIED="1430140076823" TEXT="sus potencias especiales">
 <icon BUILTIN="idea"/>
-<node CREATED="1427340565482" ID="Freemind_Link_1171041403" MODIFIED="1428510453272" TEXT="potencia de base 0">
+<node CREATED="1427340565482" FOLDED="true" ID="Freemind_Link_1171041403" MODIFIED="1430140094773" TEXT="potencia de base 0">
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1427543865069" ID="Freemind_Link_662983368" MODIFIED="1428510921428">
 <richcontent TYPE="NODE"><html>
@@ -44,11 +41,10 @@
   <body>
     <img src="a%20a%20la%200.png" />
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
-<node CREATED="1427543203871" ID="Freemind_Link_966181315" MODIFIED="1428510478906" TEXT="potencia de base 1">
+<node CREATED="1427543203871" FOLDED="true" ID="Freemind_Link_966181315" MODIFIED="1430140095662" TEXT="potencia de base 1">
 <node CREATED="1427543989729" ID="Freemind_Link_971462914" MODIFIED="1428510933541">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -57,11 +53,10 @@
   <body>
     <img src="1%20a%20la%20a.png" />
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
-<node CREATED="1427543272201" ID="Freemind_Link_1657793484" MODIFIED="1428510480256" TEXT="potencia de exponente 1">
+<node CREATED="1427543272201" FOLDED="true" ID="Freemind_Link_1657793484" MODIFIED="1430140098775" TEXT="potencia de exponente 1">
 <node CREATED="1427544100089" ID="Freemind_Link_1378557835" MODIFIED="1428510950352">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -70,11 +65,10 @@
   <body>
     <img src="a%20a%20la%201.png" />
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
-<node CREATED="1427543284948" ID="Freemind_Link_152483123" MODIFIED="1428510481270" TEXT="potencia de exponente 0">
+<node CREATED="1427543284948" FOLDED="true" ID="Freemind_Link_152483123" MODIFIED="1430140100011" TEXT="potencia de exponente 0">
 <node CREATED="1427544202840" ID="Freemind_Link_8715526" MODIFIED="1428510960538">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -83,26 +77,25 @@
   <body>
     <img src="a%20a%20la%200.png" />
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
 </node>
 <node CREATED="1424884857851" HGAP="23" ID="ID_798125870" MODIFIED="1427544278720" TEXT="Radicaci&#xf3;n" VSHIFT="2">
-<node CREATED="1427340445288" ID="Freemind_Link_1355866699" MODIFIED="1427340456792" TEXT="definici&#xf3;n">
+<node CREATED="1427340445288" ID="Freemind_Link_1355866699" MODIFIED="1428712707116" TEXT="se define">
 <icon BUILTIN="idea"/>
-<node CREATED="1427340470030" ID="Freemind_Link_671587226" MODIFIED="1428510844363" TEXT="operaci&#xf3;n inversa de la poenciaci&#xf3;n"/>
+<node CREATED="1427340470030" ID="Freemind_Link_671587226" MODIFIED="1428712730306" TEXT="operaci&#xf3;n inversa de la poenciaci&#xf3;n."/>
 </node>
-<node CREATED="1427865798513" ID="Freemind_Link_1798735751" MODIFIED="1427865809497" TEXT="simbolizaci&#xf3;n">
+<node CREATED="1427865798513" ID="Freemind_Link_1798735751" MODIFIED="1428712719842" TEXT="se simboliza">
 <icon BUILTIN="idea"/>
-<node CREATED="1427865845678" ID="Freemind_Link_1828862180" MODIFIED="1428510972425">
+<node CREATED="1427865845678" ID="Freemind_Link_1828862180" MODIFIED="1430140067805">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
-    <img src="Raiz.png" />
+    <img src="MA_05_03_060A.gif" />
   </body>
 </html>
 </richcontent>
@@ -110,31 +103,31 @@
 </node>
 <node CREATED="1424885426230" ID="ID_1648599769" MODIFIED="1428510598061" TEXT="sus t&#xe9;rminos son">
 <icon BUILTIN="idea"/>
-<node CREATED="1424885449210" FOLDED="true" ID="ID_1437493014" MODIFIED="1428510986143" TEXT="radicando, b ">
-<node CREATED="1427866005244" ID="Freemind_Link_1436834414" MODIFIED="1427866010748" TEXT="b"/>
+<node CREATED="1424885449210" ID="ID_1437493014" MODIFIED="1430139988628" TEXT="radicando, b ">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
 </node>
-<node CREATED="1424885452921" FOLDED="true" ID="ID_995601476" MODIFIED="1428510991441" TEXT="&#xed;ndice, n">
-<node CREATED="1427866012971" ID="Freemind_Link_895759351" MODIFIED="1427866015910" TEXT="n"/>
+<node CREATED="1424885452921" ID="ID_995601476" MODIFIED="1430139983907" TEXT="&#xed;ndice, n">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
 </node>
-<node CREATED="1424885455875" FOLDED="true" ID="ID_669277369" MODIFIED="1428510997781" TEXT="ra&#xed;z, a">
-<node CREATED="1427866018145" ID="Freemind_Link_1955105119" MODIFIED="1427866021480" TEXT="a"/>
+<node CREATED="1424885455875" ID="ID_669277369" MODIFIED="1430139980436" TEXT="ra&#xed;z, a">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
 </node>
 <node CREATED="1424885055304" ID="ID_1991235337" MODIFIED="1427544287964" TEXT="Logaritmaci&#xf3;n">
-<node CREATED="1427340645452" ID="Freemind_Link_407272999" MODIFIED="1427340656925" TEXT="definici&#xf3;n">
+<node CREATED="1427340645452" ID="Freemind_Link_407272999" MODIFIED="1428712760953" TEXT="se define">
 <icon BUILTIN="idea"/>
-<node CREATED="1427340660663" ID="Freemind_Link_281253497" MODIFIED="1428510868324" TEXT="operaci&#xf3;n inversa de la potenciaci&#xf3;n"/>
+<node CREATED="1427340660663" ID="Freemind_Link_281253497" MODIFIED="1428712735854" TEXT="operaci&#xf3;n inversa de la potenciaci&#xf3;n."/>
 </node>
-<node CREATED="1427866027661" ID="Freemind_Link_1132511904" MODIFIED="1427866038886" TEXT="simbolizaci&#xf3;n">
+<node CREATED="1427866027661" ID="Freemind_Link_1132511904" MODIFIED="1428712773517" TEXT="se simboliza">
 <icon BUILTIN="idea"/>
-<node CREATED="1427866070657" ID="Freemind_Link_1507183973" MODIFIED="1428511010497">
+<node CREATED="1427866070657" ID="Freemind_Link_1507183973" MODIFIED="1430139865442">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
-    <img src="Logaritmo.png" />
+    <img src="MA_05_03_060.gif" />
   </body>
 </html>
 </richcontent>
@@ -142,9 +135,15 @@
 </node>
 <node CREATED="1424885580057" ID="ID_1199312437" MODIFIED="1428510664552" TEXT="sus t&#xe9;rminos son">
 <icon BUILTIN="idea"/>
-<node CREATED="1427340209403" ID="_" MODIFIED="1428510706420" TEXT="base, a"/>
-<node CREATED="1427340225200" ID="Freemind_Link_588349835" MODIFIED="1428510692413" TEXT="argumento, b"/>
-<node CREATED="1427340238821" ID="Freemind_Link_1051030233" MODIFIED="1428510701665" TEXT="logaritmo, n"/>
+<node CREATED="1427340209403" ID="_" MODIFIED="1430139887770" TEXT="base, a">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1427340225200" ID="Freemind_Link_588349835" MODIFIED="1430139892503" TEXT="argumento, b">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1427340238821" ID="Freemind_Link_1051030233" MODIFIED="1430139897461" TEXT="logaritmo, n">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
 </node>
 </node>
 </node>
