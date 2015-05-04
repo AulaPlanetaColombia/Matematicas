abc5fcbe1dba589ead8f85b3537a81d10595d6c1
JohannaMontejo <jemontejor@pedagogica.edu.co>
2015-05-03 23:01:02 -0500

    Versión autora

---
+++ fuentes/contenidos/grado04/guion04/MA_04_04_CO_REC420.mm
@@ -0,0 +1,101 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1430014432569" ID="ID_161661116" MODIFIED="1430016165915" TEXT="N&#xfa;meros fraccionarios">
+<node CREATED="1430016218828" ID="ID_1872242096" MODIFIED="1430016246237" POSITION="right" TEXT="Representan la parte que se toma de una unidad, todo o total. ">
+<node CREATED="1430016249330" ID="ID_565262776" MODIFIED="1430016258678" TEXT="T&#xe9;rminos de una fracci&#xf3;n">
+<node CREATED="1430016260064" ID="ID_459767965" MODIFIED="1430017197030">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="fracci&#xf3;n.png" />
+  </body>
+</html></richcontent>
+<node CREATED="1430016337594" ID="ID_569799095" MODIFIED="1430016370877" TEXT="Numerador: indica la cantidad de partes iguales que se toman de la unidad. "/>
+<node CREATED="1430016373936" ID="ID_1704285205" MODIFIED="1430016418886" TEXT="Denominador: indica el n&#xfa;mero de partes iguales en que se ha dividido la unidad."/>
+</node>
+</node>
+<node CREATED="1430016427761" ID="ID_1378271260" MODIFIED="1430016436544" TEXT="Tipos de fracciones">
+<node CREATED="1430016437329" ID="ID_1350725670" MODIFIED="1430016443191" TEXT="Seg&#xfa;n su valor">
+<node CREATED="1430016444012" FOLDED="true" ID="ID_1883336089" MODIFIED="1430016510137" TEXT="Propias: representan una cantidad menor que la unidad">
+<node CREATED="1430016468657" ID="ID_1008398335" MODIFIED="1430016474937" TEXT="3/8">
+<node CREATED="1430016475622" ID="ID_1217922986" MODIFIED="1430016487775" TEXT="El numerador es menor que el denominador"/>
+</node>
+</node>
+<node CREATED="1430016490230" ID="ID_1855107747" MODIFIED="1430016520470" TEXT="Iguales a la unidad: representan la unidad completa">
+<node CREATED="1430016521102" ID="ID_1354368349" MODIFIED="1430016526451" TEXT="9/9">
+<node CREATED="1430016527192" ID="ID_405281810" MODIFIED="1430016540239" TEXT="El numerador es igual al denominador. "/>
+</node>
+</node>
+<node CREATED="1430016542379" ID="ID_678503320" MODIFIED="1430016558752" TEXT="Impropias: representan una cantidad mayor a la unidad">
+<node CREATED="1430016559228" ID="ID_746495555" MODIFIED="1430016574820" TEXT="13/5">
+<node CREATED="1430016575526" ID="ID_1614017858" MODIFIED="1430016631761" STYLE="fork" TEXT="El numerador es mayor que el denominador">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node CREATED="1430016833384" ID="ID_1102633109" MODIFIED="1430016860797" TEXT="Expresan ">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1430016863115" ID="ID_302683217" MODIFIED="1430016915757" TEXT="N&#xfa;meros mixtos">
+<node CREATED="1430016916729" ID="ID_1261949024" MODIFIED="1430017148859">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="n&#xfa;mero%20mixto.png" />
+      N: n&#250;mero natural <i>n</i>: numerador y <i>d</i>: denominador
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1430017243880" ID="ID_1975314051" MODIFIED="1430017262656" TEXT="Seg&#xfa;n sus denominadores">
+<node CREATED="1430017263788" ID="ID_1231168029" MODIFIED="1430017269319" TEXT="Homog&#xe9;neas">
+<node CREATED="1430017269886" ID="ID_664601564" MODIFIED="1430017323015" TEXT="Tienen el mismo denominador">
+<node CREATED="1430017324073" ID="ID_1156800093" MODIFIED="1430017349029" TEXT="7/12 ; 9/12 ; 23/12 ; 4/12"/>
+</node>
+</node>
+<node CREATED="1430017351034" ID="ID_1524140479" MODIFIED="1430017356542" TEXT="Heterog&#xe9;neas">
+<node CREATED="1430017357631" ID="ID_534752166" MODIFIED="1430017365760" TEXT="Tienen diferentes denominadores">
+<node CREATED="1430017366700" ID="ID_1759116389" MODIFIED="1430017407671" TEXT="3/5 ; 9/2 ; 8/10 ; 4/7"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1430017410180" ID="ID_1122298827" MODIFIED="1430017477730" TEXT="Comparaci&#xf3;n de fracciones">
+<node CREATED="1430017420417" ID="ID_1845863445" MODIFIED="1430017428059" TEXT="Homog&#xe9;neas">
+<node CREATED="1430017429114" ID="ID_1298761020" MODIFIED="1430017459978" TEXT="Es mayor, la fracci&#xf3;n que tenga el mayor numerador"/>
+</node>
+<node CREATED="1430017480321" ID="ID_1096673291" MODIFIED="1430017493186" TEXT="Heterog&#xe9;neas">
+<node CREATED="1430017494179" ID="ID_103203893" MODIFIED="1430017517670" TEXT="Hallar fracciones equivalentes homog&#xe9;neas y comparar."/>
+</node>
+</node>
+<node CREATED="1430017520233" ID="ID_1302807367" MODIFIED="1430017618521" TEXT="Operaciones ">
+<node CREATED="1430017534526" ID="ID_704087787" MODIFIED="1430017540822" TEXT="Adici&#xf3;n y sustracci&#xf3;n ">
+<node CREATED="1430017541501" ID="ID_1035381521" MODIFIED="1430017546599" TEXT="Homog&#xe9;neas">
+<node CREATED="1430017547446" ID="ID_633879762" MODIFIED="1430017572539" TEXT="Sumar o restar numerador y se mantiene el mismo denominador"/>
+</node>
+<node CREATED="1430017575041" ID="ID_726196346" MODIFIED="1430017582713" TEXT="Heterog&#xe9;neas">
+<node CREATED="1430017583583" ID="ID_1264785168" MODIFIED="1430017616503" TEXT="Hallar fracciones equivalentes homog&#xe9;neas. Sumar o restar fracciones homog&#xe9;neas."/>
+</node>
+</node>
+<node CREATED="1430017618524" ID="ID_937647396" MODIFIED="1430017623240" TEXT="Multiplicaci&#xf3;n">
+<node CREATED="1430017623982" ID="ID_1434090890" MODIFIED="1430017655377" TEXT="Multiplicar numerador por numerador y denominador por denominador"/>
+<node CREATED="1430017658406" ID="ID_974891826" MODIFIED="1430017664598" TEXT="Fracci&#xf3;n de un n&#xfa;mero">
+<node CREATED="1430017666044" ID="ID_441921387" MODIFIED="1430017685595" TEXT="Multiplicar n&#xfa;mero por numerador y dividir por denominador."/>
+</node>
+</node>
+<node CREATED="1430017687749" ID="ID_927631219" MODIFIED="1430017690926" TEXT="Divisi&#xf3;n">
+<node CREATED="1430017691620" ID="ID_1164527837" MODIFIED="1430017708553" TEXT="Multiplicar en cruz. ">
+<node CREATED="1430017709402" ID="ID_1548225994" MODIFIED="1430017745110" TEXT="Numerador por denominador y denominador por numerador. "/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
