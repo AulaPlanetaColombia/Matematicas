2650b3bd5c547b8ebe662d4b7707ab6638f9b50b
dmgonzalezm <dm.gonzalez92@yahoo.es>
2015-07-30 19:50:08 -0500

    Mapa conceptual MA_06_04_210

---
+++ fuentes/contenidos/grado06/guion04/Mapa conceptual MA_06_04_210.mm
@@ -0,0 +1,67 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1431983790028" ID="ID_1461688517" MODIFIED="1431984040489" TEXT="TEORIA DE N&#xda;MEROS ">
+<node CREATED="1431983820517" ID="ID_112494108" MODIFIED="1431983827530" POSITION="right" TEXT="Multiplos">
+<node CREATED="1431983857176" ID="ID_1660771199" MODIFIED="1431983941119" TEXT="multiplicaci&#xf3;n de un n&#xfa;mero por otro  n&#xfa;mero natural."/>
+</node>
+<node CREATED="1431983831243" ID="ID_559746896" MODIFIED="1431983835034" POSITION="left" TEXT="Divisores ">
+<node CREATED="1431983949983" ID="ID_1197773053" MODIFIED="1431984006700" TEXT="&#x9;Un n&#xfa;mero b es divisor de un n&#xfa;mero a si la divisi&#xf3;n de a entre b es exacta"/>
+<node CREATED="1431984160063" ID="ID_1460933556" MODIFIED="1431984165042" TEXT="Tiene">
+<icon BUILTIN="idea"/>
+<node CREATED="1431984168708" ID="ID_1140022384" MODIFIED="1431984178961" TEXT="Criterios de divisibilidad ">
+<node CREATED="1431984182357" ID="ID_1693149928" MODIFIED="1431984190081" TEXT="Divisible entre 2 ">
+<node CREATED="1431984191298" ID="ID_1431001029" MODIFIED="1431984205341" TEXT="termina en n&#xfa;mero par"/>
+</node>
+<node CREATED="1431984207324" ID="ID_366875054" MODIFIED="1431984214282" TEXT="Divisible entre 3 ">
+<node CREATED="1431984217121" ID="ID_1798118207" MODIFIED="1431984235595" TEXT="La suma de sus cifras es m&#xfa;ltiplo de 3"/>
+</node>
+<node CREATED="1431984239201" ID="ID_1468830891" MODIFIED="1431984246486" TEXT="Divisible entre 4 ">
+<node CREATED="1431984248935" ID="ID_548371742" MODIFIED="1431984278410" TEXT="sus dos &#xfa;ltimas cifras forman un m&#xfa;ltiplo de 4 o son ceros"/>
+</node>
+<node CREATED="1431984283074" ID="ID_391684359" MODIFIED="1431984290315" TEXT="Divisible entre 5">
+<node CREATED="1431984293684" ID="ID_372543506" MODIFIED="1431984304717" TEXT="termina en 0 o 5"/>
+</node>
+<node CREATED="1431984307603" ID="ID_1557748370" MODIFIED="1431984313955" TEXT="Divisible entre 6">
+<node CREATED="1431984315983" ID="ID_60877235" MODIFIED="1431984329246" TEXT="es sivisible entre 2 y 3 al mismo tiempo"/>
+</node>
+<node CREATED="1431984332194" ID="ID_1427073126" MODIFIED="1431984340041" TEXT="Divisible entre 7 ">
+<node CREATED="1431984342568" ID="ID_1022356491" MODIFIED="1431984360641" TEXT="cuando al hacer  la diferencia entre el n&#xfa;mero sin la cifra de las unidades y el doble de la cifra de las unidades es 0 o m&#xfa;ltiplo de 7"/>
+</node>
+<node CREATED="1431984363870" ID="ID_1477885707" MODIFIED="1431984387276" TEXT="Divisible entre  9 ">
+<node CREATED="1431984389304" ID="ID_463410347" MODIFIED="1431984393222" TEXT="la suma de sus cifras resulta siendo m&#xfa;ltiplo tambi&#xe9;n de nueve"/>
+</node>
+<node CREATED="1431984398339" ID="ID_453856989" MODIFIED="1431984405483" TEXT="Divisible entre 10">
+<node CREATED="1431984408498" ID="ID_1563497186" MODIFIED="1431984413350" TEXT="Termina en 0"/>
+</node>
+<node CREATED="1431984416329" ID="ID_106807528" MODIFIED="1431984425167" TEXT="Divisible entre 11">
+<node CREATED="1431984427975" ID="ID_1164898986" MODIFIED="1431984447557" TEXT="la diferencia entre la suma de las cifras que ocupan los lugares pares y la de los impares es 0 o m&#xfa;ltiplo de 11"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1431983843834" HGAP="-64" ID="ID_807744559" MODIFIED="1431984045918" POSITION="right" TEXT="se clasifican en " VSHIFT="33">
+<node CREATED="1431984056581" ID="ID_1159387450" MODIFIED="1431984060419" TEXT="COMPUESTOS ">
+<node CREATED="1431984121784" ID="ID_1423953336" MODIFIED="1431984152965" TEXT="puede escribirse como el producto o la multiplicaci&#xf3;n de n&#xfa;meros naturales menores que &#xe9;l">
+<node CREATED="1431984624121" ID="ID_874683079" MODIFIED="1431984641755" TEXT="Se pueden descomponer en ">
+<icon BUILTIN="idea"/>
+<node CREATED="1431984647153" ID="ID_1539304168" MODIFIED="1431984656969" TEXT="Factores primos ">
+<node CREATED="1431984674194" ID="ID_987025175" MODIFIED="1431984684256" TEXT="Diagramas ">
+<node CREATED="1431984690563" ID="ID_973783451" MODIFIED="1431984697257" TEXT="&#xe1;rbol"/>
+<node CREATED="1431984700348" ID="ID_422928003" MODIFIED="1431984707664" TEXT="Vertical "/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1431984514312" ID="ID_211420274" MODIFIED="1431984518025" TEXT="PRIMOS ">
+<node CREATED="1431984521176" ID="ID_445005361" MODIFIED="1431984563226" TEXT="tiene exactamente dos divisores diferentes, el 1 y &#xe9;l mismo"/>
+</node>
+</node>
+<node CREATED="1431984736936" ID="ID_270642020" MODIFIED="1431984749507" POSITION="left" TEXT="MAXIMO COMUN DIVISOR ">
+<node CREATED="1431984776312" ID="ID_1900820823" MODIFIED="1431984814793" TEXT=" es el mayor de los divisores comunes de dichos n&#xfa;meros"/>
+</node>
+<node CREATED="1431984751582" HGAP="-97" ID="ID_646792449" MODIFIED="1431984763083" POSITION="right" TEXT="MINIMO COMUN MULTIPLO " VSHIFT="58">
+<node CREATED="1431984848374" ID="ID_1600429716" MODIFIED="1431984850589" TEXT="es el menor m&#xfa;ltiplo com&#xfa;n distinto de cero"/>
+</node>
+</node>
+</map>
