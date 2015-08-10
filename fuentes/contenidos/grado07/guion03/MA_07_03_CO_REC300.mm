f581bd2300211d74893fcb547f680b10ff20d061
dvelasquezrojas <diana.velasquezrojas@gmail.com>
2015-08-10 07:05:24 -0500

    Actualización guion MA_07_03_CO según escaleta
    Se realizó la actualización del cuaderno de estudio del guion
    MA_07_03_CO (sólo fue cuaderno de estudio y numeración de recursos)

---
+++ fuentes/contenidos/grado07/guion03/MA_07_03_CO_REC300.mm
@@ -0,0 +1,96 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Otras operaciones con N&#xfa;meros enteros(Z)" ID="ID_575732925" CREATED="1426883964909" MODIFIED="1426884328432"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Potenciaci&#xf3;n" POSITION="right" ID="ID_1223049483" CREATED="1426884087218" MODIFIED="1426885688933" HGAP="90" VSHIFT="-64">
+<node TEXT="es" ID="ID_1944003328" CREATED="1426884169198" MODIFIED="1426885474317" HGAP="62" VSHIFT="41">
+<node TEXT="La multiplicaci&#xf3;n de factores iguales" ID="ID_1239817664" CREATED="1426885198935" MODIFIED="1433337439206"/>
+</node>
+<node TEXT="cumple" ID="ID_88050031" CREATED="1426885251144" MODIFIED="1426885478309" HGAP="58" VSHIFT="19">
+<node TEXT="Propiedades" ID="ID_47336881" CREATED="1426885270904" MODIFIED="1426885280819">
+<node TEXT="Producto de potencias de igual base" ID="ID_360830405" CREATED="1426885284823" MODIFIED="1426885400885" HGAP="49" VSHIFT="-27"/>
+<node TEXT="Cociente de potencias de igual base" ID="ID_913113876" CREATED="1426885288775" MODIFIED="1426885421101" HGAP="49" VSHIFT="-16"/>
+<node TEXT="Potencia de una potencia" ID="ID_1230297397" CREATED="1426885292792" MODIFIED="1426885418557" HGAP="49" VSHIFT="1"/>
+<node TEXT="Potencia de un producto" ID="ID_1850383719" CREATED="1426885297240" MODIFIED="1426885444133" HGAP="48" VSHIFT="16"/>
+<node TEXT="Potencia de un cociente" ID="ID_1232283312" CREATED="1426885300807" MODIFIED="1426885462574" HGAP="47" VSHIFT="17"/>
+</node>
+</node>
+<node TEXT="se combina con otras operaciones para hacer" ID="ID_1235214733" CREATED="1426885693416" MODIFIED="1426886188813" HGAP="91" VSHIFT="-4">
+<node TEXT="Polinomios" ID="ID_302307793" CREATED="1426885983696" MODIFIED="1426886214644">
+<node TEXT="cuya resoluci&#xf3;n depende de" ID="ID_101445244" CREATED="1426885993656" MODIFIED="1433337358167">
+<node TEXT="Los signos de agrupaci&#xf3;n" ID="ID_782849042" CREATED="1426886002055" MODIFIED="1433337413868" HGAP="31" VSHIFT="-14"/>
+<node TEXT="La jerarqu&#xed;a de las operaciones" ID="ID_1725584140" CREATED="1426886005827" MODIFIED="1433337420608" HGAP="37" VSHIFT="3"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Radicaci&#xf3;n" POSITION="right" ID="ID_440039709" CREATED="1426884111361" MODIFIED="1433337365473" HGAP="112" VSHIFT="-15">
+<node TEXT="es " ID="ID_616586966" CREATED="1426884361334" MODIFIED="1433337365473" HGAP="56" VSHIFT="9">
+<node TEXT="La operaci&#xf3;n inversa de la potenciaci&#xf3;n" ID="ID_1943621040" CREATED="1426884405687" MODIFIED="1433337375114" HGAP="51" VSHIFT="-1"/>
+</node>
+<node TEXT="cumple" ID="ID_1686537832" CREATED="1426884460975" MODIFIED="1426884874077" HGAP="56" VSHIFT="15">
+<node TEXT="Propiedades" ID="ID_1558724530" CREATED="1426884478854" MODIFIED="1426884549413" HGAP="38" VSHIFT="-3">
+<node TEXT="Ra&#xed;z de un producto" ID="ID_394692079" CREATED="1426884564184" MODIFIED="1426885506949" HGAP="33" VSHIFT="-4"/>
+<node TEXT="Ra&#xed;z de un cociente" ID="ID_221794252" CREATED="1426884648400" MODIFIED="1426885510397" HGAP="34" VSHIFT="-1"/>
+<node TEXT="Ra&#xed;z de una ra&#xed;z" ID="ID_1880047223" CREATED="1426884666616" MODIFIED="1426885513877" HGAP="35" VSHIFT="8"/>
+<node TEXT="Ra&#xed;z de una potencia" ID="ID_1480199849" CREATED="1426884699304" MODIFIED="1426885518413" HGAP="34" VSHIFT="6"/>
+</node>
+</node>
+<node TEXT="se combina con otras operaciones para hacer" ID="ID_1392901908" CREATED="1426886169096" MODIFIED="1426886258373" HGAP="66" VSHIFT="-9">
+<node TEXT="Polinomios" ID="ID_360976854" CREATED="1426886205856" MODIFIED="1426886225396">
+<node TEXT="cuya resoluci&#xf3;n depende de" ID="ID_446134071" CREATED="1426886228863" MODIFIED="1433337395170">
+<node TEXT="Los signos de agrupaci&#xf3;n" ID="ID_1238947365" CREATED="1426886245527" MODIFIED="1433337404368" HGAP="27" VSHIFT="-20"/>
+<node TEXT="La jerarqu&#xed;a de las operaciones" ID="ID_1782314184" CREATED="1426886250223" MODIFIED="1433337409555" HGAP="30" VSHIFT="3"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
