5de3e09d88ea626cccc3c93dad57371373352b3e
Oliver Castelblanco Martínez <ocastelblanco@gmail.com>
2015-11-30 15:53:29 -0500

    Eliminación de contenidos de primaria

--- fuentes/contenidos/grado03/guion01/MA_03_01_CO_REC120.mm
+++
@@ -1,4 +0,0 @@
-<map version="1.0.1">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1426558393093" ID="ID_387308243" MODIFIED="1426558514787" TEXT="Conjuntos"/>
-</map>
